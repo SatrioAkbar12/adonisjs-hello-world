4a21270d80947c47238ed5d6d2288cb1c808537b
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2025-11-30 12:47:54 +0700

    add routes for login and logout

--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,22 @@
 |
 */
 
+import SessionController from '#controllers/session_controller'
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
+  }
+})
+
+router.post('/session', [SessionController, 'store']) // for login
+router.delete('/session', [SessionController, 'destroy']).use(middleware.auth()) // for logout
+
+router.get('/dashboard', async () => {
+  return {
+    status: 'success',
+    message: 'You are authenticated.'
   }
 })
